7a271d2898ed704408aab778504a96b7c34dba74
Lancine223 <lacinekeita20@gmail.com>
2022-10-12 08:10:03 +0000

    secondexo

---
+++ second.py
@@ -0,0 +1,13 @@
+#EXO 2 La table de multiplication
+n = int(input("Entrer un nombre pour voir sa table de multiplication"))
+print(f"""La table de multiplication de {n} est \n
+      {n} * 1 = {n*1}\n
+      {n} * 2 = {n*2}\n
+      {n} * 3 = {n*3}\n
+      {n} * 4 = {n*4}\n
+      {n} * 5 = {n*5}\n
+      {n} * 6 = {n*6}\n
+      {n} * 7 = {n*7}\n
+      {n} * 8 = {n*8}\n
+      {n} * 9 = {n*9}\n
+      {n} * 10 = {n*10}""")
